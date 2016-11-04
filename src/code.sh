93dfa54cd6288a51e46ca3912067d5535da745ca
Aled Jones <aledjones@nhs.net>
2016-11-04 11:10:40 +0000

    change output to qc

--- src/code.sh
+++ src/code.sh
@@ -48,7 +48,16 @@ $java -jar /CalculateHsMetrics.jar BI=$targets.picard TI=$targets.picard I="$nam
 name=`dx describe "${sorted_bam}" --name`
 name="${name%.bam}"
 
-file_id=`dx upload hsmetrics.tsv -o "$name.hsmetrics.tsv" --brief`
-dx-jobutil-add-output "hsmetrics_tsv" "$file_id"
-file_id=`dx upload pertarget_coverage.tsv -o "$name.pertarget_coverage.tsv" --brief`
-dx-jobutil-add-output "pertarget_coverage_tsv" "$file_id"
+dx ls
+ls
+
+mkdir -p ~/out/hsmetrics_tsv/QC/ ~/out/pertarget_coverage_tsv/QC/
+mv hsmetrics.tsv ~/out/hsmetrics_tsv/QC/"$name.hsmetrics.tsv"
+mv pertarget_coverage.tsv ~/out/pertarget_coverage_tsv/QC/"$name.pertarget_coverage.tsv"
+
+dx-upload-all-outputs --parallel
+
+# file_id=`dx upload hsmetrics.tsv -o "$name.hsmetrics.tsv" --brief`
+# dx-jobutil-add-output "hsmetrics_tsv" "$file_id"
+# file_id=`dx upload pertarget_coverage.tsv -o "$name.pertarget_coverage.tsv" --brief`
+# dx-jobutil-add-output "pertarget_coverage_tsv" "$file_id"
