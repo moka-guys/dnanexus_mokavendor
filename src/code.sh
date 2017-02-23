007a5a639e4fe6225d177ce7911c96ed86d62ef1
aledj2 <aledjones@nhs.net>
2017-02-23 15:11:50 +0000

    ready for custom panels

--- src/code.sh
+++ src/code.sh
@@ -8,38 +8,40 @@ set -e -x -o pipefail
 mem_in_mb=`head -n1 /proc/meminfo | awk '{print int($2*0.9/1024)}'`
 java="java -Xmx${mem_in_mb}m"
 
-#
-# Fetch mappings
-#
+
+#get the filename
 name=`dx describe "${sorted_bam}" --name`
+#remove .bam
 name="${name%.bam}"
-#dx download "$sorted_bam" -o input.bam
+
+# download input bam and bed files
 dx download "$sorted_bam" -o "$name.bam"
 dx download "$vendor_exome_bedfile"
 
-#
+#give .sh script permissions to run
+chmod +x /usr/bin/reconstruct-human-genome.sh 
 # Fetch / reconstruct genome, and optionally index it
-#
 genome=`reconstruct-human-genome.sh "$name.bam"`
 if [ ! -e genome.fa.fai ]; then
   samtools faidx genome.fa
 fi
 
-#
 # Fetch and prepare regions
-#
 #appdata=project-B6JG85Z2J35vb6Z7pQ9Q02j8
 targets=${vendor_exome_bedfile_prefix}_${genome}_targets
 #dx download "$appdata:/vendor_exomes/$targets.bed" 
 
+
 samtools view -H "$name.bam" | grep '^@SQ' > $targets.picard
+
+#recreate the vendor bedfile format from mokabed files
 cat $vendor_exome_bedfile_prefix.bed | grep -v '^#' | sed 's/chr//' | awk -F '\t' '{print $1,$2,$3}' >  tidied.bed
 
 awk '{print $1 "\t" $2+1 "\t" $3 "\t+\t" $1 ":" $2+1 "-" $3}' < tidied.bed >> $targets.picard
 
-head tidied.bed
-head $vendor_exome_bedfile_prefix.bed
-head $targets.picard -n 100
+#head tidied.bed
+#head $vendor_exome_bedfile_prefix.bed
+#head $targets.picard -n 100
 
 #
 # Run Picard CalculateHsMetrics
